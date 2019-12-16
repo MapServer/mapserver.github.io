9ea0a9204e20c17a04adfd430f1f8c48446db87d
MapServer deploybot <mapserverbot@mapserver.bot>
2019-12-16 19:00:27 +0000

    update with results of commit https://github.com/mapserver/docs/commit/17212a0ff820e8766d64db9300118e6617dfc7a4

--- tr/_static/documentation_options.js
+++ tr/_static/documentation_options.js
@@ -1,8 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '7.4.2',
+    VERSION: '7.4.3',
     LANGUAGE: 'tr',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
