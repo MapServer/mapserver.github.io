a5b85b28a43662f365ce97fd1635e0de6dc526e9
MapServer deploybot <mapserverbot@mapserver.bot>
2024-11-01 09:21:48 +0000

    update with results of commit https://github.com/mapserver/MapServer-documentation/commit/6035981

--- id/_static/documentation_options.js
+++ id/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '8.0.1',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '8.2.2',
     LANGUAGE: 'id',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
